12d07814adb080213852cb348c6e4e1c87238875
Mecronorm <alessandropaap@hotmail.com>
2023-10-31 10:44:25 +0100

    Finnish part 5

--- Events/script.js
+++ Events/script.js
@@ -15,10 +15,41 @@ const clickOnSquare = (e) => {
   logEntry.append(logText);
   main.children[0].append(newSquare)
   main.children[2].children[1].append(logEntry)
+  const generatedSquares = document.querySelectorAll(".displayedsquare")
+  for (let generatedSquare of generatedSquares) {
+  generatedSquare.addEventListener("click", alertbox)
+}
+}
+
+const alertbox = (a) => {
+   window.alert(a.target.classList[0])
 }
 
 const actionSquares = document.querySelectorAll('.actionsquare')
 for (let actionSquare of actionSquares) {
   actionSquare.addEventListener('click', clickOnSquare)
 }
+
+
+
+document.body.addEventListener("keypress", event => {
+  if (event.code === "Space") {
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    document.body.style.backgroundColor = "#" + randomColor;
+    const logEntry = document.createElement("li");
+    const logText = document.createTextNode("[" + getElapsedTime() + "] Generated a new backgroundcolor")
+    logEntry.append(logText);
+    main.children[2].children[1].append(logEntry)
+  }
+  if (event.code === "KeyL") {
+    while (main.children[2].children[1].children[0] != 0) {
+      main.children[2].children[1].children[0].remove();
+    }
+  }
+  if (event.code === "KeyS") {
+    while (main.children[0].children[0] != 0) {
+      main.children[0].children[0].remove();
+    }
+  }
+})
 
