b6cdac56b493da4ff40c842e5165b210c58e05a3
Mecronorm <alessandropaap@hotmail.com>
2023-11-02 10:34:40 +0100

    Finnish excercise 7

--- DOM-drill/script.js
+++ DOM-drill/script.js
@@ -32,8 +32,58 @@ document.body.addEventListener("keyup", (event) => {
         console.log(shuffledArr)
         shuffledArr.forEach(element => {
             if (element.localName === "li") {
-                console.log(element)
-                favList.append(element)
+                if (element.className === "important") {
+                    console.log(element)
+                    favList.append(element)
+                }
+            }
+        });
+        shuffledArr.forEach(element => {
+            if (element.localName === "li") {
+                if (element.className != "important") {
+                    console.log(element)
+                    favList.append(element)
+                }
+            }
+        });
+    }
+    if (event.code === "KeyD") {
+        const node = favList.firstElementChild
+        const clone = node.cloneNode(true);
+        favList.insertBefore(clone, favList.children[0]);
+    }
+})
+
+const newDiv = document.createElement("div")
+document.body.insertBefore(newDiv, document.body.children[1])
+const newSelect = document.createElement("select")
+newDiv.append(newSelect)
+const optionOne = document.createElement("option")
+const optionTwo = document.createElement("option")
+optionOne.innerHTML = "important franchises"
+optionTwo.innerHTML = "normal franchises"
+optionOne.value = "important"
+optionTwo.value = "normal"
+newSelect.append(optionTwo)
+newSelect.append(optionOne)
+
+newSelect.addEventListener("change", (event) => {
+    if (event.target.value === "important") {
+        listItems.forEach(element => {
+            if (element.nodeType === 1) {
+                if (element.className != "important") {
+                    element.style.visibility = "hidden";
+                }
+            }
+        });
+    }
+    if (event.target.value === "normal") {
+        
+        listItems.forEach(element => {
+            if (element.nodeType === 1) {
+                if (element.className != "important") {
+                    element.style.visibility = "visible";
+                }
             }
         });
     }
